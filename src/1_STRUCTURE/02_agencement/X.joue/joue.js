540945a0915d924e326cd1b398ec5d1c2f2dab4d
Erwin67000 <philae.menuiserie@gmail.com>
2026-09-07 10:54:55 +0200

    Update joue.js

--- src/1_STRUCTURE/02_agencement/X.joue/joue.js
+++ src/1_STRUCTURE/02_agencement/X.joue/joue.js
@@ -1,4 +1,4 @@
 /**
  * Joue (panneau X) — géométrie via PANNEAU_DEFS + buildPanneauComplet.
  */
-export { buildPanneauComplet as buildJoue } from '../../00_matrice/matrice_panneau_grok.js'
+export { buildPanneauComplet as buildJoue } from '../../00_matrice/matrice_panneau.js'
